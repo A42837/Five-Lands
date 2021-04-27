90b090f223250531fbae95ced582db54865409c6
passadeira <joaomiguelpassos@gmail.com>
2021-04-27 17:26:25 +0100

    Companion: ataque à distância

--- Five Lands/Five Lands/Assets/Scripts/Companion/Companion_Behavior.cs
+++ Five Lands/Five Lands/Assets/Scripts/Companion/Companion_Behavior.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using RPG.Attributes;
+using RPG.Combat;
 
 namespace RPG.Companion
 {
@@ -8,11 +9,16 @@ namespace RPG.Companion
     {
 
         [SerializeField] Transform player;
+        [SerializeField] Projectile projectile;
+        [SerializeField] Transform mouth;
+
         bool isAttacking = false, isChasing = false, isTargetAlive;
         float rotSpeed = 3.0f, moveSpeed = 4.0f;
+
+        Health targetHealth;
+
         GameObject[] enemies;
         GameObject target;
-        float targetHealth;
 
 
         void Start()
@@ -21,7 +27,43 @@ namespace RPG.Companion
         }
 
         void Update()
+        {
+            if (!isAttacking)
+            {
+                foreach (GameObject enemy in enemies)
+                {
+                    if (Vector3.Distance(enemy.transform.position, transform.position) <= 15)
+                    {
+                        if (enemy.GetComponent<Health>().GetHealthPoints() > 0)
+                        {
+                            isTargetAlive = true;
+                            isAttacking = true;
+                            target = enemy;
+                            break;
+                        }
+                    }
+                }
 
+                if (Vector3.Distance(transform.position, player.position) > 2) Follow();
+
+                else GetComponent<Animator>().SetFloat("Speed", 0);
+            }
+
+            else if (isTargetAlive)
+            {
+                targetHealth = target.GetComponent<Health>();
+                if (targetHealth.GetHealthPoints() <= 0)
+                {
+                    isTargetAlive = false;
+                    isAttacking = false;
+                }
+                else if (Vector3.Distance(target.transform.position, transform.position) > 10) Chase();
+                else Attack();
+            }
+        }
+
+
+        /*void Update()
         {
             if (!isAttacking)
             {
@@ -54,7 +96,8 @@ namespace RPG.Companion
                 else if (Vector3.Distance(target.transform.position, transform.position) > 2) Chase();
                 else Attack();
             }
-        }
+        }*/
+
 
         private void Follow()
         {
@@ -79,6 +122,12 @@ namespace RPG.Companion
             GetComponent<Animator>().SetFloat("Speed", 0);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), rotSpeed * Time.deltaTime);
             GetComponent<Animator>().SetTrigger("Attack");
+        }
+
+        public void SpitVenom()
+        {
+            Projectile projectileInstance = Instantiate(projectile, mouth.position, Quaternion.identity);
+            projectileInstance.setTarget(targetHealth, target, 0f);
         }
 
     }
