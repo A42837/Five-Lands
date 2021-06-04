89703dfb4f7d38f1799940f6cbc68aac0131b670
A42837 <72973385+A42837@users.noreply.github.com>
2021-06-04 13:20:28 +0100

    fodasse erros so erros caralho

--- Five Lands/Five Lands/Assets/Scripts/Attributes/HealthDisplay.cs
+++ Five Lands/Five Lands/Assets/Scripts/Attributes/HealthDisplay.cs
@@ -20,7 +20,6 @@ namespace RPG.Attributes
         {
             //0:0 quer dizer que a primeira variavel depois da virgula, vai ser mostrada com 0 casas decimais !
             GetComponent<Text>().text = string.Format("{0:0}/{1:0}", health.GetHealthPoints(), health.GetMaxHealthPoints() );
-            
         }
 
 
